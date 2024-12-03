131908cc2acb15cd72c206103f9a6a3d66330cbb
akashsingh596 <72357922+akashsingh596@users.noreply.github.com>
2024-12-04 01:59:45 +0530

    Add refresh Token

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,6 +3,7 @@ import {
   registerUser,
   loginUser,
   logoutUser,
+  refreshAccessToken,
 } from "../controllers/user.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
@@ -28,5 +29,7 @@ router.route("/login").post(loginUser);
 // secured Routes
 
 router.route("/logout").post(verifyJWT, logoutUser);
+
+router.route("/refresh-token").post(refreshAccessToken);
 
 export default router;
